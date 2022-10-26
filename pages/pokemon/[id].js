cbb0aa6fafb5ac93efbba70cb6e57cae4da3e12a
Hassan  Samy Elhamaky <hsamy@ntgclarity.com>
2022-10-26 13:37:57 +0200

    pokemon page

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -1,8 +1,27 @@
 import { useRouter } from 'next/router';
+import Head from 'next/head';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import styles from '../../styles/Details.module.css';
 
 export default function Details() {
   const {
     query: { id },
   } = useRouter();
-  return <div> Hello {id}</div>;
+
+  const [pokemon, setPokemon] = useState({});
+
+  useEffect(() => {
+    async function getPokemon() {
+      const resp = await fetch(
+        `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${id}.json`
+      );
+      setPokemon(await resp.json());
+    }
+    if (id) {
+      getPokemon();
+    }
+  }, [id]);
+
+  return <div> {JSON.stringify(pokemon)}</div>;
 }
